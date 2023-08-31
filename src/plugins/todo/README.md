2cda22e9414b6fa37876fd0a739d11955995817c
d-rigel <alozie4God@gmail.com>
2023-08-31 12:50:37 -0700

    first commit

---
+++ src/plugins/todo/README.md
@@ -0,0 +1,7 @@
+# Strapi plugin todo
+
+A quick description of todo.
+
+To start the project locally RUN yarn install and use your env credentials to to fill your env file.
+
+After installation and all RUN "yarn run develop --watch-admin" to start the project
---
+++ src/plugins/todo/admin/src/app.js
@@ -0,0 +1,12 @@
+import TweetButton from './extensions/components/TweetButton'; // Component displaying a tweet button in the Content Manager
+
+export default {
+  // ...
+  bootstrap(app) {
+    app.injectContentManagerComponent('editView', 'right-links', {
+      name: 'TweetButton',
+      Component: TweetButton,
+    });
+  },
+  // ...
+};
