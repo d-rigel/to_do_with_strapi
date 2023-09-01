6c41fde9b0e34ff968c0f830a70f804e6e20bab8
d-rigel <alozie4God@gmail.com>
2023-09-01 10:40:55 -0700

    admin content manager button bug fix

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,12 +1,14 @@
-// import TweetButton from './extensions/components/TweetButton'; // Component displaying a tweet button in the Content Manager
+// Component displaying a tweet button in the Content Manager
+import TweetButton from "./extensions/components/TweetButton"
 
-// export default {
-//   // ...
-//   bootstrap(app) {
-//     app.injectContentManagerComponent('editView', 'right-links', {
-//     //   name: 'TweetButton',
-//     //   Component: TweetButton,
-//     });
-//   },
-//   // ...
-// };
+
+export default {
+  // ...
+  bootstrap(app) {
+    app.injectContentManagerComponent('editView', 'right-links', {
+      name: 'TweetButton',
+      Component: TweetButton,
+    });
+  },
+  // ...
+};
